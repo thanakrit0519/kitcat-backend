4a29fef91c9fc484872061567126a0e6df1f0a08
Thanapat Jonglertthiti <thanajong@outlook.com>
2023-09-20 09:11:46 +0700

    loginapi200923

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './register/register.module';
-
+import { LoginModule } from './login/login.module';
 @Module({
-  imports: [RegisterModule],
+  imports: [RegisterModule,LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
